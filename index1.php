c014b4ac75b962af2f46826b40cfc53997745dc8
zamzammmm <zamzamzuba2017@gmail.com>
2019-08-03 13:09:58 +0700

    Add PhpDan Html

--- index1.php
+++ index1.php
@@ -4,59 +4,40 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>PHP Dan Html</title>
 </head>
 <body>
-<FORM ACTION="" METHOD="POST" NAME="input">
-Masukan angka : <input type="text" name="angka">
+<?php
+$a = 100;
+$b = 2;
+$c = $a * $b;
+?>
+      <h1 style="color: cyan">
+      <!-- ISI VARIABLE -->
+    <?php
+    echo "$a";
+    ?>
+     </h1>
+
+      <h1 style="color: Blue">
+      <!-- ISI VARIABLE -->
+    <?php
+    echo "$b";
+    ?>
+     </h1>
+
+      <h1 style="color: lime">
+      <!-- ISI VARIABLE -->
+    <?php
+    echo "Hasilnya Adalah : <b>$c<b>";
+    ?>
+     </h1>
+
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
 </body>
 </html>
-<?php
-if (isset($_GET['input'])) {
-    $angka = $_POST['angka'];
-    echo "anda memasukan angka $angka";
-    if ($angka == "1") {
-        echo "anda memasukan warna biru";
-        echo "<hr width=10px color='blue' >";
-    }
-    elseif ($angka == "2") {
-        echo "anda memasukan warna merah";
-        echo "<hr width=10px color='red' >";
-    }
-    elseif ($angka == "3") {
-        echo "anda memasukan warna hijau";
-        echo "<hr width=10px color='green' >";
-    }
-    elseif ($angka == "4") {
-        echo "anda memasukan warna kuning";
-        echo "<hr width=10px color='yellow' >";
-    }
-    elseif ($angka == "5") {
-        echo "anda memasukan warna biru";
-        echo "<hr width=10px color='blue' >";
-    }
-    elseif ($angka == "6") {
-        echo "anda memasukan warna biru";
-        echo "<hr width=10px color='blue' >";
-    }
-    elseif ($angka == "7") {
-        echo "anda memasukan warna nigga";
-        echo "<hr width=10px color='#000000' >";
-    }
-    elseif ($angka == "8") {
-        echo "anda memasukan warna orange";
-        echo "<hr width=10px color='#ff8000' >";
-    }
-    elseif ($angka == "9") {
-        echo "anda memasukan warna pink";
-        echo "<hr width=10px color='#ff00ff' >";
-    }
-    elseif ($angka == "10") {
-        echo "anda memasukan warna lime";
-        echo "<hr width=10px color='#66ff33' >";
-    }
-    else {
-        echo "Maaf,Nomor yang anda masukan Invalid karena anda wibu";
-    }
-}
-?>
