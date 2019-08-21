3f7919549ec0889d23f7daca76adc5bc143a62da
zamzammmm <zamzamzuba2017@gmail.com>
2019-08-21 07:26:01 +0700

    Menambahkan File loop1

--- loop2.php
+++ loop2.php
@@ -2,12 +2,12 @@
 for ($i= 1; $i <= 10; $i++) {
     echo"Perkalian $i<br><br>";
     for ($a= 1; $a <= 10; $a++) {
-        $r=$a*$i;
-        if ($r%2==1) {
-            echo $a. "x".$i." = ".$r." Bilangan Ganjil<br>";
+        $z=$a*$i;
+        if ($z%2==1) {
+            echo $a. "x".$i." = ".$z." Bilangan Ganjil<br>";
         }
         else {
-            echo $a. "x".$i." = ".$r." Bilangan Genap<br>";
+            echo $a. "x".$i." = ".$z." Bilangan Genap<br>";
         }
     }
     echo"<br>";
